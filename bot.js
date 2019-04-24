80df3ad47c6af69b08d76937199bca62268af745
Awebdo <49924103+Awebdo@users.noreply.github.com>
2019-04-24 13:32:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,29 @@
-const discord = require ('discord.js');
+const Discord = require('discord.js');
+
 const client = new Discord.Client();
+
+ 
+
 client.on('ready', () => {
-client.on('i am ready!');
+
+    console.log('I am ready!');
+
 });
+
+ 
+
 client.on('message', message => {
-if (message.content === 'ping') {
-message.reply('pong!');
-}
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
-client.login(process.env.BOT_TOKEN) ://where BOT_TOKEN is the token of our bot
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
