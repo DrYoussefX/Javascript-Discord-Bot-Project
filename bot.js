dad8880f969f045a97e0fa43220ecee1f7f5c510
Awebdo <49924103+Awebdo@users.noreply.github.com>
2019-04-24 16:56:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const prefix = "$";
+
  
 
 client.on('ready', () => {
@@ -9,6 +11,13 @@ client.on('ready', () => {
     console.log('I am ready!');
 
 });
+
+client.on('message', message => {
+    if(message.content.startsWith("$ping")) {
+            message.channel.send('Pong! Your ping is ' +${Date.now() - message.createdTimestamp}+ ' ms')
+}
+    });
+
 
  
 
@@ -28,8 +37,3 @@ client.on('message', message => {
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
 
-client.on('message', message => {
-    if(message.content.startsWith("$ping")) {
-            message.channel.send('Pong! Your ping is ' +${Date.now() - message.createdTimestamp}+ ' ms')
-}
-    });
