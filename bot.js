997287191ac289a8b641ff9dca9a12c0b36706d9
Awebdo <49924103+Awebdo@users.noreply.github.com>
2019-04-30 22:55:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const prefix = "-";
 client.on('ready', () => {
 
     console.log('I am ready!')
-client.user.setGame('testing set presence')
+client.user.setGame('testing set presence');
 
 });
 client.on('message', message => {
