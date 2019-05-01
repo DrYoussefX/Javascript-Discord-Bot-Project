c0401ccf15464543349a461fdc4ad620f66c4cef
Awebdo <49924103+Awebdo@users.noreply.github.com>
2019-05-01 16:19:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,6 @@ client.user.setPresence({ game: { name: 'with discord.js' }, status: 'idle' })
 });
 client.on('message', message => {
 	 if (message.content.startsWith(prefix + "restart")) {
-		 message.reply("restarting...")
 		 process.exit(1);
 }
 });
