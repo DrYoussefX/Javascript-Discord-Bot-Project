1f4aadcae1541e141a8fc30c0ecddfc527c6cc34
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-18 00:56:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,5 +66,30 @@ client.on("message", message => {
       client.on("messageDelete", (messageDelete) => {
         messageDelete.guild.channels.find(channel => channel.name == "activity").send(`__***DELETED MESSAGE***__ : "**${messageDelete.content}**" by **${messageDelete.author.tag}** sent in ${messageDelete.channel} was deleted.`)
        });
-    
+    client.on("message", message => {
+	      if (message.content.startsWith(prefix + "verify")) {
+		      if (message.channel.name !== 'verification') return message.delete();
+	      if (message.member.roles.some(role => role.name === 'Verified')) {
+		      message.channel.send("You're already verified.");
+	      } else {
+	      let num = Math.floor((Math.random() * 4783) + 10);    
+     message.channel.send(`Please type the following number : **${num}**`).then(m => {     
+         message.channel.awaitMessages(res => res.content == `${num}`, {       
+           max: 1,     
+           time: 30000,    
+           errors: ['time'],       
+         }).then(collected => {    
+          message.channel.bulkDelete(3)
+message.channel.send(`${message.author.username} was successfully verified :white_check_mark:`)
+           message.author.sendMessage(`Hey, ${message.author} you were automatically verified by me! You now have permissions to the main server channels. \n \nEnjoy! :wink:`);
+		 message.member.addRole(message.guild.roles.find(c => c.name == "Verified"));     
+ 
+ 
+          }).catch(() => {     
+           m.edit(`You took too long to type the number.\nRe-type the command again if you want to verify yourself.`).then(m2 => m.delete(15000));     
+       });
+	    })
+		      }
+          }
+        });
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
