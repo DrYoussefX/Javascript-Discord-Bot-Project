dc1e54a7363848c10e6f1f702c12afadf0815498
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-18 00:46:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,32 +14,7 @@ client.user.setPresence({ game: { name: 'with discord.js' }, status: 'idle' })
 
 });	
 
-    client.on("message", message => {
-	      if (message.content.startsWith(prefix + "verify")) {
-		      if (message.channel.name !== 'verification') return message.delete();
-	      if (message.member.roles.some(role => role.name === 'Verified')) {
-		      message.channel.send("You're already verified.");
-	      } else {
-	      let num = Math.floor((Math.random() * 4783) + 10);    
-     message.channel.send(`Please type the following number : **${num}**`).then(m => {     
-         message.channel.awaitMessages(res => res.content == `${num}`, {       
-           max: 1,     
-           time: 30000,    
-           errors: ['time'],       
-         }).then(collected => {    
-          message.channel.bulkDelete(3)
-message.channel.send(`${message.author.username} was successfully verified :white_check_mark:`)
-           message.author.sendMessage(`Hey, ${message.author} you were automatically verified by me! You now have permissions to the main server channels. \n \nEnjoy! :wink:`);
-		 message.member.addRole(message.guild.roles.find(c => c.name == "Verified"));     
- 
- 
-          }).catch(() => {     
-           m.edit(`You took too long to type the number.\nRe-type the command again if you want to verify yourself.`).then(m2 => m.delete(15000));     
-       });
-	    })
-		      }
-          }
-        });
+   
         client.on('message', message=> {
           if(message.content.includes('احا') || message.content.includes('شرموط') || message.content.includes('عرص') || message.content.includes('خول') || message.content.includes('زبي')){
             message.delete(100);
