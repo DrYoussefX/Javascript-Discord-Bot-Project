327bcee00a2e1de74d87a6afd5f6da41624ac0a9
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-18 00:54:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,25 +66,5 @@ client.on("message", message => {
       client.on("messageDelete", (messageDelete) => {
         messageDelete.guild.channels.find(channel => channel.name == "activity").send(`__***DELETED MESSAGE***__ : "**${messageDelete.content}**" by **${messageDelete.author.tag}** sent in ${messageDelete.channel} was deleted.`)
        });
-       client.on("message", message=>{
-         if(message.content.startsWith("-mybmi")) {
-            let weight = message.content.split(' ').slice(1).slice(3).join (' '); // Remember arrays are 0-based!.
-            let height = message.content.split(' ').slice(1).slice(2).join(' ');
-            let result = weight/height;
-            message.reply(`Hello ${message.author.username}, I see you're a ${weight} year old ${height} from ${result} . Wanna date?`);
-          }
-        });
-       client.on("message", message=>{
-         if(message.content.startsWith(">info")) {
-           var mentionedUser = message.mentions.members.first()
-           var embedFee = new Discord.RichEmbed()
-           .setAuthor(`${mentionedUser.user.username}`, `${mentionedUser.user.avatarURL}`)
-           .setColor('#0099ff')
-           .setDescription(`**>Information of** __**${mentionedUser.user.username}**__(${mentionedUser.id})\n**Username:** ${mentionedUser.user.tag}\n**User ID:** ${mentionedUser.id}\n**User Status:** ${mentionedUser.user.presence.status}\n**Create:** ${mentionedUser.user.createdAt} \n \n __**>Member information**__\n **Joined At:** ${mentionedUser.joinedAt}`)
-           .setFooter(`Info requested by ${message.author.username} (${message.author.id})`)
-           message.channel.sendEmbed(embedFee)
-         }
-       })
     
-
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
