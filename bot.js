92169bc040d2d34470c85438b02963fc13e25eed
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-18 00:41:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,28 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const prefix = "-";
-
  
 
 client.on('ready', () => {
 
-    console.log('I am ready!')
+    console.log('I am ready!');
+
 });
+
+ 
+
 client.on('message', message => {
-if(message.content.includes("lol") {
-   message.reply("lmao");
-   })
-}
-client.login(process.env.BOT_TOKEN);
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
