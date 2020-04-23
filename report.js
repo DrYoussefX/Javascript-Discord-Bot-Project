7fa433f0a654860768b0e479074add7f25a981a0
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-23 23:22:15 +0200

    Add files via upload

---
+++ report.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const reportSchema = mongoose.Schema({
+    _id: String,
+    username: String,
+    userID: String,
+    reason: String,
+    rUsername: String,
+    rID: String,
+    time: String,
+    warnID: String
+});
+module.exports = mongoose.model("Report", reportSchema)
