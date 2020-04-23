9301ee08d249cb8ce7ee6823579e14ba40652632
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-23 23:28:29 +0200

    Create delwarn.js

---
+++ commands/delwarn.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const Report = require("../report.js")
+const mongoose = require("mongoose");
+module.exports.run = async (bot, message, args) => {
+    mongoose.connect('mongodb+srv://joealex:el7etan1@cluster0-hv0fc.mongodb.net/Reports?retryWrites=true&w=majority');
+    let id = args[0]
+    Report.findOneAndDelete({warnID: id}, function(err, report) {
+        if(!report) return message.channel.send("This ID is not valid.")
+        var delembed = new Discord.RichEmbed()
+      .setTitle("Warn notification")
+      .setDescription(`Warn ID **${id}** logged for **${report.username}** was successfully deleted.`)
+      .setColor('#0099ff')
+      message.channel.sendEmbed(delembed)
+    })
+}
+module.exports.help = {
+    name: "delwarn"
+}
