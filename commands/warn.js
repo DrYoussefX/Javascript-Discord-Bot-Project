cd08b64a166678a73f3a5fd273afa0a107d2996e
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-23 23:27:26 +0200

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,41 @@
+const discord = require("discord.js");
+const Report = require("../report.js")
+const mongoose = require("mongoose");
+module.exports.run = async (bot, message, args) => {
+    await message.delete();
+    
+    
+    mongoose.connect('mongodb+srv://joealex:el7etan1@cluster0-hv0fc.mongodb.net/Reports?retryWrites=true&w=majority');
+    let rUser = message.mentions.members.first();
+    if(!rUser) return message.reply("Couldn't find the mentioned member.");
+    let rreason = args.slice(1).join(" ");
+    if(!rreason) return message.reply("Provide a reason")
+    let warnsID = Math.floor((Math.random() * 4783) + 10);  
+    
+const report = new Report({
+    _id: mongoose.Types.ObjectId(),
+    username: rUser.user.username,
+    userID: rUser.id,
+    reason: rreason,
+    rUsername: message.author.username,
+    rID: message.author.id,
+    time: message.createdAt,
+    warnID: warnsID
+});
+report.save()
+.then(result => console.log(result))
+.catch(err => console.log(err));
+var embed = new discord.RichEmbed()
+    .setTitle("Warn Notification")
+    .setDescription(`**Warn successfully logged for ${report.username}** (${report.userID})\n**Reason :-** ${report.reason}\n**Warned By :-** ${report.rUsername} (${report.rID})\n **Warned At :-** ${report.time}\n**Warn ID :-**${report.warnID}`)
+    .setColor('#0099ff')
+    message.channel.sendEmbed(embed)
+}
+
+
+
+
+
+module.exports.help = {
+name: "warn"
+}
