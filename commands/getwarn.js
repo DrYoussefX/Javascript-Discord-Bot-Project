ee6d1e70bdfe4ed769a91ab2cbee2c0aaa408fe2
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-27 23:03:39 +0200

    Update getwarn.js

--- commands/getwarn.js
+++ commands/getwarn.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const Report = require("../report.js")
 const mongoose = require("mongoose");
 module.exports.run = async (bot, message, args) => {
-  mongoose.connect('mongodb+srv://joealex:el7etan1@cluster0-hv0fc.mongodb.net/Reports?retryWrites=true&w=majority');
+  mongoose.connect('mongodb+srv://joealex:el7etan1@cluster0-hv0fc.mongodb.net/Actionss?retryWrites=true&w=majority');
     //let rUser = message.mentions.members.first();
     let id = args[0]
     Report.findOne({warnID: id}, function(err, report) {
