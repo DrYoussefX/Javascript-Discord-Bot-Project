626553f44610bbaee3fcd087ee31ba9227b1a675
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-28 12:37:50 +0200

    Create prefix.js

---
+++ commands/prefix.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const Config = require("../config.js")
+const mongoose = require("mongoose");
+module.exports.run = async (bot, message, args) => {
+    //await message.delete();
+    mongoose.connect('mongodb+srv://joealex:el7etan1@cluster0-hv0fc.mongodb.net/Prefix?retryWrites=true&w=majority');
+    let prefix = args[1]
+    const config = new Config({
+        guildID: message.guild.id,
+        prefix: prefix
+    });
+    config.save()
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+}
+module.exports.run = {
+    name: "setprefix"
+}
