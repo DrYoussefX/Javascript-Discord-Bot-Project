b1bd3962fbdd882050fef8206ca4647eeff02566
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-28 13:58:47 +0200

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,14 +4,10 @@ const mongoose = require("mongoose");
 module.exports.run = async (bot, message, args) => {
     //await message.delete();
     mongoose.connect('mongodb+srv://joealex:el7etan1@cluster0-hv0fc.mongodb.net/Prefix?retryWrites=true&w=majority');
-    let prefix = args[0]
-    const config = new Config({
-        guildID: message.guild.id,
-        prefix: prefix
-    });
-    config.save()
-    .then(result => console.log(result))
-    .catch(err => console.log(err));
+    let prefix = args[1]
+    Config.findOneAndUpdate({guildID: message.guild.id}, {prefix: prefix}, function(err, config) {
+        message.channel.send(`Prefix was successfully changed to ${config.prefix}`)
+    })
 }
 module.exports.help = {
     name: "setprefix"
