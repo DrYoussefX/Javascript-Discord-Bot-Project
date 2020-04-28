27a3c588851046d8cd40be8205f9e4a47857e94a
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-28 22:18:14 +0200

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     mongoose.connect('mongodb+srv://joealex:el7etan1@cluster0-hv0fc.mongodb.net/Prefix?retryWrites=true&w=majority');
     mongoose.set('useFindAndModify', false);
     let prefix = args[0]
-    if(!prefix) return message.channel.send("No prefix found")
+    //if(!prefix) return message.channel.send("No prefix found")
     Config.findOneAndUpdate({guildID: message.guild.id}, {prefix: prefix}, function(err, config) {
         if(err) console.log(err)
         message.channel.send(`Prefix was successfully changed.`)
