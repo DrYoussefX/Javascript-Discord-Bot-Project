15da6aaec69616979ab94173ec46ba9ba76f1bcf
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-04-29 14:26:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,5 +65,14 @@ if(message.content.startsWith(">prefix")) {
  })
 }
 })
-
+bot.on("message", message => {
+  mongoose.connect('mongodb+srv://joealex:el7etan1@cluster0-hv0fc.mongodb.net/Prefix?retryWrites=true&w=majority');
+    mongoose.set('useFindAndModify', false);
+    let prefix = args[0]
+    if(!prefix) return message.channel.send("No prefix found")
+    Config.findOneAndUpdate({guildID: message.guild.id}, {prefix: prefix}, function(err, config) {
+        if(err) console.log(err)
+        message.channel.send(`Prefix was successfully changed.`)
+    })
+})
 bot.login(process.env.BOT_TOKEN);
