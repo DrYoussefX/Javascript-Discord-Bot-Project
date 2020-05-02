b05b3a19afd9fe6ac2d79c0661ef2a92b71ad96e
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-03 01:55:15 +0200

    Update verification.js

--- commands/verification.js
+++ commands/verification.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 
 module.exports.run = async (bot, message, args) => {
-    if (message.author.roles.some(role => role.name === 'Verified')) return message.channel.send("You're already verified")
+    //if (message.author.roles.some(role => role.name === 'Verified')) return message.channel.send("You're already verified")
     let msg = await message.guild.channels.get('706280557033553980').send(`User ${message.author.username} has requested verification. Would you like to verify him?`)
     
     msg.react("👍")
