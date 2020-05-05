3290ffab1c0e7a01451bcd6ac659375edbf293b8
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-05 02:24:03 +0200

    Update banusers.js

--- commands/banusers.js
+++ commands/banusers.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    if(message.author.id !== "546316934187057163") return;
+    //if(message.author.id !== "546316934187057163") return;
     let member = message.mentions.members.first()
     member.ban()
     .then(console.log)
