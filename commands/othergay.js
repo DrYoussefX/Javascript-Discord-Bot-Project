c8ca2267ea7c12b21e0df2917f7ef112ad758550
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-10 01:43:31 +0200

    Update othergay.js

--- commands/othergay.js
+++ commands/othergay.js
@@ -5,7 +5,8 @@ let gayness = Math.floor((Math.random() * 100) + 10);
 let gayEmbed = new Discord.RichEmbed()
 .setTitle("Gayness Rate")
 .setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
-.setDescription(`${gay.username} is ${gayness}% gay :gay_pride_flag:`)
+.setDescription(`${gay.user.username} is ${gayness}% gay :gay_pride_flag:`)
+message.channel.sendEmbed(gayEmbed)
 }
 module.exports.help = {
     name: "gay"
