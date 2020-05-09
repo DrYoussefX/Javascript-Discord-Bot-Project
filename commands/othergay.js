725f036a3890a8717c70fd13c21f5a08d232a008
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-10 01:40:25 +0200

    Create othergay.js

---
+++ commands/othergay.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    let gay = message.mentions.members.first()
+let gayness = Math.floor((Math.random() * 100) + 10);
+let gayEmbed = new Discord.RichEmbed()
+.setTitle("Gayness Rate")
+.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
+.setDescription(`${gay.username} is ${gayness}% gay :gay_pride_flag:`)
+}
+module.exports.help = {
+    name: "gay"
+}
