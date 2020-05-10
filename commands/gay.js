ba1b35131869b11f54a4b7ff4950c88f31c44c18
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-10 14:41:53 +0200

    Update gay.js

--- commands/gay.js
+++ commands/gay.js
@@ -1,12 +1,22 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    //let gay = message.mentions.members.first()
-let gayness = Math.floor((Math.random() * 100) + 10);
+    let gay = message.mentions.members.first()
+let gayness = Math.floor((Math.random() * 90) + 10);
 let gayEmbed = new Discord.RichEmbed()
 .setTitle("Gayness Rate")
 .setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
 .setDescription(`${message.author.username} is ${gayness}% gay :gay_pride_flag:`)
+.setColor('#9400D3')
+if(!gay) {
 message.channel.sendEmbed(gayEmbed)
+} else {
+    let gayEmbedO = new Discord.RichEmbed()
+.setTitle("Gayness Rate")
+.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
+.setDescription(`${gay.user.username} is ${gayness}% gay :gay_pride_flag:`)
+.setColor('#9400D3')
+message.channel.sendEmbed(gayEmbedO)
+}
 }
 module.exports.help = {
     name: "gay"
