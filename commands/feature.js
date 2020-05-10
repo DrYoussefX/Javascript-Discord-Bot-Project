edb40974366032294e48a52188fe9f505aadcced
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-10 15:04:00 +0200

    Create feature.js

---
+++ commands/feature.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    let update = new discord.RichEmbed()
+    .setTitle("Update Notification")
+    .setFooter(`New update released by Drdery.`)
+  .setDescription(args.slice(">feature").join(" "))
+  .setColor("#9400D3")
+   message.channel.sendEmbed(update)
+}
+module.exports.help = {
+    name: "feature"
+}
