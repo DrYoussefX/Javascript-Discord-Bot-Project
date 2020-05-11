63a10728360302df0db8ea87b7a01d940d8f3120
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-11 04:11:33 +0200

    Update feature.js

--- commands/feature.js
+++ commands/feature.js
@@ -2,6 +2,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
    let msg = args.slice(">say").join(" ")
    message.channel.send(msg)
+   message.delete();
 }
 module.exports.help = {
     name: "say"
