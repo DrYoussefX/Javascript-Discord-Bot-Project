0a3ecf2343cade80f269a97d5add02d1ae0dce6f
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-11 04:10:41 +0200

    Update feature.js

--- commands/feature.js
+++ commands/feature.js
@@ -1,12 +1,8 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    let update = new discord.RichEmbed()
-    .setTitle("Update Notification")
-    .setFooter(`New update released by Drdery`)
-  .setDescription(args.slice(">feature").join(" "))
-  .setColor("#9400D3")
-   message.channel.sendEmbed(update)
+   let msg = args.slice(">say").join(" ")
+   message.channel.send(msg)
 }
 module.exports.help = {
-    name: "feature"
+    name: "say"
 }
