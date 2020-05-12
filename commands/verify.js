1d5f40ac347b7be041b06481ce17e4f68078a972
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-12 03:46:58 +0200

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -17,3 +17,23 @@ message.channel.send("You were successfully verified")
 module.exports.help = {
     name: "verify"
 }
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+let verifyno = Math.floor((Math.random() * 5000) + 100);
+message.channel.send(`Type the verification code :- **${verifyno}**, you have`)
+const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+console.log(collector)
+collector.on('collect', message => {
+    if(message.content == verifyno) {
+message.channel.send("You were successfully verified")
+message.member.addRole(message.guild.roles.find(c => c.name == "Verified"));
+    } else {
+        if(message.contet !== verifyno) {
+            message.channel.send("You entered a wrong verification code.")
+        }
+    }
+    })
+}
+module.exports.help = {
+    name: "verify"
+}
