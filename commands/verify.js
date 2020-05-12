c07df697e60d3d0a05599780a8386f8d8820bdd2
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-12 16:38:25 +0200

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const bot = new discord.Client({disableEveryone: true});
 module.exports.run = async (bot, message, args) => {
 let verifyno = Math.floor((Math.random() * 5000) + 100);
     if (message.member.roles.some(role => role.name === 'Verified')) return message.channel.send("You're already verified.")
@@ -6,12 +7,22 @@ message.channel.send(`Type the verification code :- **${verifyno}** .`)
 const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
 console.log(collector)
 collector.on('collect', message => {
-    if(message.content == verifyno) {
-message.channel.send("You were successfully verified")
-message.member.addRole(message.guild.roles.find(c => c.name == "Verified"));
-    } else {
+    const collector1 = new Discord.MessageCollector();
         if(message.contet !== verifyno) {
             message.channel.send("You entered a wrong verification code.")
+        } else {
+            if(message.content == verifyno) {
+                message.channel.send(`You've succesfully passed the first stage of verification. Your request must be accepted by server admins.`)
+               // message.member.addRole(message.guild.roles.find(c => c.name == "Verified"));
+               bot.channels.get("709775175331217519").send(`User ${message.author.username} has request verification, would you like to verify him? (y/n)`)
+               collector.on('collect', msg => {
+                   if(msg.content == "y") {
+                    message.member.addRole(message.guild.roles.find(c => c.name == "Verified"));
+                    message.channel.send("Your request was successfully verified.")
+
+                   }
+               })
+                    
         }
     }
     })
