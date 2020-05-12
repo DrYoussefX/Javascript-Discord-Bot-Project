30d77fe96dfd2284e29559d08b4e5ca5d1c9da90
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-12 03:26:08 +0200

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    //let verified = message.mentions.members.first()
 let verifyno = Math.floor((Math.random() * 5000) + 100);
-message.channel.send(`Type the verification code :- **${verifyno}, you have`)
+message.channel.send(`Type the verification code :- **${verifyno}**, you have`)
 const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
 console.log(collector)
 collector.on('collect', message => {
-    if(message.content === verifyno) {
+    if(message.content == verifyno) {
 message.channel.send("You were successfully verified")
     } else {
         if(message.contet !== verifyno) {
