42e106677d1defee71cf4acfeaae17af12442135
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-12 17:16:20 +0200

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -3,22 +3,24 @@ const bot = new Discord.Client({disableEveryone: true});
 module.exports.run = async (bot, message, args) => {
 let verifyno = Math.floor((Math.random() * 5000) + 100);
     if (message.member.roles.some(role => role.name === 'Verified')) return message.channel.send("You're already verified.")
+    message.delete(1000)
 message.channel.send(`Type the verification code :- **${verifyno}** .`)
 const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
 console.log(collector)
 collector.on('collect', message => {            
     if(message.content == verifyno) {
     message.channel.send(`You've succesfully passed the first stage of verification. Your request must be accepted by server admins.`)
-   // 
+   message.delete(1000)
    const filter = (reaction, user) => reaction.emoji.name === '👍' && user.id === '546316934187057163'
    let channel = bot.channels.get("709775175331217519")
-   channel.send(`User ${message.author.username} has request verification, would you like to verify him? (y/n)`).then(sentmsg => {
+   channel.send(`User ${message.author.username} has requested verification, would you like to verify him? (y/n)`).then(sentmsg => {
    sentmsg.react("👍")
    sentmsg.react("👎")
    sentmsg.awaitReactions(filter, { time: 15000 })
 .then(collected => {
     channel.send(`${message.author.username} was given the **Verified** role.`)
     message.member.addRole(message.guild.roles.find(c => c.name == "Verified"));
+    sentmsg.delete(1000)
 })
    })
       
