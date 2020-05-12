d46a50ebfe522065fcad740014be3f1ed26004a6
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-12 17:24:06 +0200

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -22,7 +22,13 @@ collector.on('collect', message => {
     message.member.addRole(message.guild.roles.find(c => c.name == "Verified"));
     sentmsg.delete(1000)
 })
-   })
+const filter1 = (reaction, user) => reaction.emoji.name === '👎' && user.id === '546316934187057163'   
+sentmsg.awaitReactions(filter, { time : 15000 })
+.then(collected => {
+    channel.send(`${message.author.username}'s verification request was rejected`)
+    sentmsg.delete(1000)
+})
+})
       
         } else {
             if(message.contet !== verifyno) {
