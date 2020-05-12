22edc111b070c2f01984afc988dceab455875e8a
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-05-12 02:52:06 +0200

    Create verify.js

---
+++ commands/verify.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    let verified = message.mentions.members.first()
+let verifyno = Math.floor((Math.random() * 5000) + 100);
+message.channel.send(`Hey ${message.author}, would you like to get verified at ${message.guild.name}`)
+const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+console.log(collector)
+collector.on('collect', message => {
+    if(message.content === "y") {
+    message.channel.send(`Alright, you have 20 seconds to type the follwoing number :- **${verifyno}**.`)
+    } else {
+        if(message.content === "n") {
+            message.channel.send(`Alright, as you wish.`)
+        }
+    }
+})
+}
+module.exports.help = {
+    name: "verify"
+}
