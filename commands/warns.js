ac8f0b0abf578e9b5c3415e5b1e6a5a8ae5715ed
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-06-13 00:02:10 +0200

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -15,7 +15,7 @@ module.exports.run = async(bot, message, args) => {
         __v: false,
         username: false
     };
-    Report.find({username: rUser.user.username}, usersProjection, {lean:true}, function(err, report) {
+    Report.aggregate({username: rUser.user.username}, usersProjection, function(err, report) {
         var userMap = {};
     
         report.forEach(function(report) {
