bbc9e6b3954b6e96a009aa80025d9f6e8254d6a5
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-07-15 09:51:23 +0200

    Update gay.js

--- commands/gay.js
+++ commands/gay.js
@@ -5,7 +5,7 @@ let gayness = Math.floor((Math.random() * 90) + 10);
 let gayEmbed = new Discord.RichEmbed()
 .setTitle("Gayness Rate")
 
-.setAuthor(`${message.author.username}`, `${message.author.avatarURL}`
+.setAuthor(`${message.author.username}`, `${message.author.avatarURL}`)
 //.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
 .setDescription(`${message.author.username} is ${gayness}% gay :gay_pride_flag:`)
 .setColor('#9400D3')
@@ -14,7 +14,7 @@ message.channel.sendEmbed(gayEmbed)
 } else {
     let gayEmbedO = new Discord.RichEmbed()
 .setTitle("Gayness Rate")
-.setAuthor(`${gay.user.username}`, `${gay.user.avatarURL}`
+.setAuthor(`${gay.user.username}`, `${gay.user.avatarURL}`)
 //.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
 .setDescription(`${gay.user.username} is ${gayness}% gay :gay_pride_flag:`)
 .setColor('#9400D3')
