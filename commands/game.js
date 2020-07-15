b485ba8236dd169d3d19d199cd7bf51a51d2ca9c
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-07-15 12:49:34 +0200

    Update game.js

--- commands/game.js
+++ commands/game.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 var move = args[0]
 var facts = ["Rock", "Paper", "Scissors"];
 var fact = Math.floor(Math.random() * facts.length); 
-  if(move == "Paper" {
+  if(move == "Paper") {
      if(facts[fact] == "Paper") {
 message.channel.send("Paper, its a Draw.")
     } else {
