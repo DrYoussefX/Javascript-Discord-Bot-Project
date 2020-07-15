ee7a83a9d971936ed32bfae61e4ee9e5cfe44ffd
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-07-15 09:57:16 +0200

    Update gay.js

--- commands/gay.js
+++ commands/gay.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
     let gay = message.mentions.members.first()
 let gayness = Math.floor((Math.random() * 90) + 10);
 let gayEmbed = new Discord.RichEmbed()
-.setTitle("Gayness Rate")
+//.setTitle("Gayness Rate")
 
 .setAuthor(`${message.author.username}`, `${message.author.avatarURL}`)
 //.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
@@ -13,7 +13,7 @@ if(!gay) {
 message.channel.sendEmbed(gayEmbed)
 } else {
     let gayEmbedO = new Discord.RichEmbed()
-.setTitle("Gayness Rate")
+//.setTitle("Gayness Rate")
 .setAuthor(`${gay.user.username}`, `${gay.user.avatarURL}`)
 //.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
 .setDescription(`${gay.user.username} is ${gayness}% gay :gay_pride_flag:`)
