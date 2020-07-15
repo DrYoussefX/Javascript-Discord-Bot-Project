ee312050a9e563d389f55e66b717d17a5f8f5558
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-07-15 09:49:29 +0200

    Update gay.js

--- commands/gay.js
+++ commands/gay.js
@@ -4,29 +4,8 @@ module.exports.run = async (bot, message, args) => {
 let gayness = Math.floor((Math.random() * 90) + 10);
 let gayEmbed = new Discord.RichEmbed()
 .setTitle("Gayness Rate")
-.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
-.setDescription(`${message.author.username} is ${gayness}% gay :gay_pride_flag:`)
-.setColor('#9400D3')
-if(!gay) {
-message.channel.sendEmbed(gayEmbed)
-} else {
-    let gayEmbedO = new Discord.RichEmbed()
-.setTitle("Gayness Rate")
-.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
-.setDescription(`${gay.user.username} is ${gayness}% gay :gay_pride_flag:`)
-.setColor('#9400D3')
-message.channel.sendEmbed(gayEmbedO)
-}
-}
-module.exports.help = {
-    name: "gay"
-}
-const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
-    let gay = message.mentions.members.first()
-let gayness = Math.floor((Math.random() * 90) + 10);
-let gayEmbed = new Discord.RichEmbed()
-.setTitle("Gayness Rate")
+
+.setAuthor(`${message.author.username}`, `${message.author.avatarURL}`
 //.setFooter(`Requested by ${message.author.username} || Bot programmed by :- DRDERY`)
 .setDescription(`${message.author.username} is ${gayness}% gay :gay_pride_flag:`)
 .setColor('#9400D3')
@@ -45,3 +24,4 @@ message.channel.sendEmbed(gayEmbedO)
 module.exports.help = {
     name: "gay"
 }
+
