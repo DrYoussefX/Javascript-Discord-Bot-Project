e6ac40c8c5c48fb58c15367fc86f40304d2a8577
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-07-17 10:55:57 +0200

    Update feature.js

--- commands/feature.js
+++ commands/feature.js
@@ -1,8 +1,11 @@
 const discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
-   let msg = args.slice(">say").join(" ")
-   message.channel.send(msg)
-   message.delete();
+module.exports.run = async (bot, msg, args) => {
+  let text = msg.content.slice('>say'.length); // cuts off the /private part 
+   msg.guild.members.forEach(member => { 
+      if (member.id != client.user.id && !member.user.bot) { 
+         member.send(text); 
+      }); 
+   }
 }
 module.exports.help = {
     name: "say"
