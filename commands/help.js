ec45afa155b7a36ea025683ffaf5be235d414d22
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-07-20 19:55:29 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => { 
 message.member.send(`Hey **${message.author.username}**! \n**Twoon**'s website link :- http://twoon.ga/?i=1 \n **Twooon**'s support server :- https://discord.com/invite/rB6vZt6 \nI hope you enjoy using me!`)
-message.react(":white_check_mark:")
+message.react("✅")
 }
 module.exports.help = {
 name: "help"
