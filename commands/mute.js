cb7a22266093c095d2ae7b00e0e0d5cfab9f7156
Jesse541 <49819184+Jesse541@users.noreply.github.com>
2020-07-23 17:19:07 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,24 +1,21 @@
 const Discord = require("discord.js");
-const mysql = require('mysql');
+const sqlite3 = require("sqlite3").verbose()
 const ms = require("ms");
-const connection = mysql.createConnection({
-  host     : 'sql7.freemysqlhosting.net',
-  user     : 'sql7356164',
-  password : 'mypassword',
-  database : 'sql7356164',
-  port : '3306'
-});
+let db = new sqlite3.Database('./commands/infractions.db');
+db.run("CREATE TABLE IF NOT EXISTS infractions (punishedusername TEXT, punishedid TEXT, adminusername TEXT, adminid TEXT, type TEXT, mutetime TEXT,infractionid TEXT,reason TEXT)")
 module.exports.run = async (bot, message, args) => {
+  
+  
     //await message.delete();
     let aUser = message.mentions.members.first();
    // if(!aUser) return message.reply("Couldn't find the mentioned member.");
     let mutetime = args[1]
    
-    let warnsID = Math.floor((Math.random() * 4783) + 10);
+    let infrID = Math.floor((Math.random() * 4783) + 10);
     let areason = args.slice(2).join(" ");
     if(!message.member.hasPermission('ADMINISTRATOR')) {
       var noperms = new Discord.RichEmbed()
-.setAuthor(`${message.author.username}`, `${message.author.avatarURL}`)
+.setAuthor(`${message.author.username}`)
 .setTitle("Command: Mute")
 .setColor('#9400D3')
 .setFooter(`Mute attempt by ${message.author.username} (${message.author.id})`)
@@ -30,17 +27,17 @@ message.channel.sendEmbed(noperms)
           var unmute = new Discord.RichEmbed()
           
           .setTitle("Command: Mute")
-          .setColor('#9400D3')
-          .setDescription("Mute a discord member to prevent him from sending messages in text channels!\n **USAGE** :- \n >mute [user] (Mentioned User) [time in seconds] [reason] \n **Examples** :- \n>Mute @Youssef 10s Spamming")
+          .setColor('#00d5ff')
+          .setDescription("Mute a discord member to prevent him from sending messages in text channels!\n **USAGE** :- \n >mute [user] (Mentioned User) [time in seconds] [reason] \n **Examples** :- \n>mute @Youssef 10s Spamming")
               
           message.channel.sendEmbed(unmute)
       } else {
         
       if(message.member == aUser) {
         var mutedself = new Discord.RichEmbed()
-        .setAuthor(`${aUser.user.username}`, `${aUser.user.avatarURL}`)
+        .setAuthor(`${aUser.user.username}`)
         .setTitle("Command: Mute")
-        .setColor('#9400D3')
+        .setColor('#00d5ff')
         .setFooter(`Mute attempt by ${message.author.username} (${message.author.id})`)
         .setDescription(`You can't mute yourself, ${aUser.user.username}!`);
         message.channel.sendEmbed(mutedself)
@@ -49,38 +46,35 @@ message.channel.sendEmbed(noperms)
       if(!areason) return message.reply("Provide a reason for the mute")
         if (aUser.roles.some(role => role.name === 'Muted')) {
         var unmute1 = new Discord.RichEmbed()
-        .setAuthor(`${aUser.user.username}`, `${aUser.user.avatarURL}`)
+        .setAuthor(`${aUser.user.username}`)
  .setFooter(`Mute attempt by ${message.author.username} (${message.author.id})`)
     .setTitle("Command: Mute")
-    .setColor('#9400D3')
+    .setColor('#00d5ff')
     .setDescription(`User ${aUser.user.username}  is already muted!`);
        message.channel.sendEmbed(unmute1);
     } else {
-       
-     
-      connection.run("CREATE TABLE IF NOT EXISTS mutes(username TEXT, userid TEXT, timestamp TEXT)").then(() => {
-        connection.run("INSERT INTO mutes(username, userid, timestamp) VALUES (?, ?, ?)", [message.author.username, message.author.id, message.createdAt]);
-      })
+   
+      db.run("INSERT INTO infractions(punishedusername, punishedid, adminusername, adminid, type, mutetime,infractionid,reason) VALUES(?,?,?,?,?,?,?,?)",aUser.user.username,aUser.user.id,message.author.username,message.author.id,"Mute",mutetime,infrID,areason)
          var unmuted = new Discord.RichEmbed()
-    .setAuthor(`${aUser.user.username}`, `${aUser.user.avatarURL}`)
+    .setAuthor(`${aUser.user.username}`)
     .setTitle("Command: Mute")
-    .setColor('#9400D3')
+    .setColor('#00d5ff')
    // .setFooter(`Mute done by ${message.author.username} (${message.author.id})`)
    .setDescription(`**Mute successfully logged for ${aUser.user.username}** (${aUser.user.id})\n**Reason :-** ${areason}\n**Mute Time:-** ${ms(ms(mutetime))}\n**Muted By :-** ${message.author.username} (${message.author.id})\n **Muted At :-** ${message.createdAt}`)
    var unmutedd = new Discord.RichEmbed()
-   .setAuthor(`${aUser.user.username}`, `${aUser.user.avatarURL}`)
+   .setAuthor(`${aUser.user.username}`)
     .setTitle("Command: Mute")
-    .setColor('#9400D3')
-    .setDescription(`User **${aUser.user.username}** was successfully muted for the following reason :- **${areason}**`)
+    .setColor('#00d5ff')
+    .setDescription(`**Infraction#${infrID}**\nUser **${aUser.user.username}** was successfully muted for the following reason :- **${areason}**`)
     aUser.addRole(message.guild.roles.find(c => c.name == "Muted"));
     message.channel.sendEmbed(unmutedd)
       aUser.sendEmbed(unmuted)
       setTimeout(function(){
         aUser.removeRole(message.guild.roles.find(c => c.name == "Muted"))
         var embedT = new Discord.RichEmbed()
-        .setAuthor(`${aUser.user.username}`, `${aUser.user.avatarURL}`)
+        .setAuthor(`${aUser.user.username}`)
         .setTitle("Unmute Notification")
-    .setColor('#9400D3')
+    .setColor('#00d5ff')
     .setDescription(`You were unmuted,**${aUser.user.username}**, time interval ended.`)
     aUser.sendEmbed(embedT)
         //message.channel.send("User was unmuted")
@@ -94,7 +88,7 @@ message.channel.sendEmbed(noperms)
 }
 
 
-//yy
+
 
 
 module.exports.help = {
